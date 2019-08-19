e9dbc30d67b2fc0aa078ff4f7a68fffa852ae10d
Sai <sai.s.kamat@gmail.com>
2019-08-19 14:40:57 +0200

    ash script which will launch a file to record certain data
    the recording mechanism is irrelevant, but if there exists one, it'll be inside ¨record_some_stuff.py¨

---
+++ handling_keyboard_interrupts_f01.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# this is a bash script which will launch a file to record certain data.
+# the recording mechanism is irrelevant, but if there exists one, it'll be inside ¨record_some_stuff.py¨
+echo 'Entering recording script...'
+
+python record_some_stuff.py
+
+echo 'Exited scipt.'
