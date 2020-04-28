6e68fb47635421258b7fa4311119587b67f8fcbf
Sai <sai.s.kamat@gmail.com>
2020-04-28 13:31:34 +0200

    Update download_file_from_url.py

--- download_file_from_url.py
+++ download_file_from_url.py
@@ -6,25 +6,21 @@ import urllib.request
 import subprocess
 import csv
 import operator
-from csv import writer
+from urllib.parse import urlparse
+#from csv import writer
+#import os
 # import pandas as pd
 # filename = "Vrbo_highscore_samples"
 filename = "test_csv"
 Good_Count = 2488
 Bad_Count = 2741
-# newfilename="new_file_url"
 
-# def sort_by_scores(filename):
-#     # df = pd.read_csv("{0}.csv".format(filename))
-#     df = pd.read_csv('test_url.csv')
-#     print(df.head())
-#     df = df.sort_values('aesthetic_score')
-#     df.to_csv("{0}.csv".format(newfilename), index=False)
-    
-# with open('test_url.csv') as fp:
-#     crdr = csv.reader(fp)
-#     # set key1 and key2 to column numbers of keys
-#     filedata = sorted(crdr, key=lambda row: (row['aesthetic_score']))
+def extract_filename(url):
+    a = urlparse(url)
+    # print(a.path)                    
+    return (os.path.basename(a.path))
+
+
 def download_from_csv(filename):
     with open("{0}.csv".format(filename), 'r') as csvfile:
         i = 0
@@ -33,12 +29,14 @@ def download_from_csv(filename):
         # check whether we have image URL
             if splitted_line[0] != '' and splitted_line[0] != "\n" and splitted_line[0] != 'url':
                 try:
-                    urllib.request.urlretrieve(splitted_line[0], filename + "_" + str(i) + ".jpg")
+                    saved_file = extract_filename(splitted_line[0])
+                    # print(saved_file)
+                    # urllib.request.urlretrieve(splitted_line[0], filename + "_" + str(i) + ".jpg")
+                    urllib.request.urlretrieve(splitted_line[0], saved_file)
                 except:
                     print('**** 404 Error at :', splitted_line[0])
                 else:
                     print ("Image saved for {0}".format(splitted_line[0]))
-
                 finally:
                     i+=1
             else:
@@ -56,6 +54,7 @@ def make_datasets(filename):
     subprocess.call(["mkdir smartly/negatives"])
     subprocess.call(["cd smartly/", filename])
     subprocess.call(["ls | sort -R | tail ",(0.35*Bad_Count)," | while read file; do mv $file ../negatives/ ; done"])
+
 
 # sort_by_scores(filename)
 download_from_csv(filename)
