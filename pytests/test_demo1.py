6c6d3cd5244c6d9d909d1d386b1cf63248324a96
saikamat <sai.s.kamat@gmail.com>
2023-09-18 18:01:55 +0200

    pytests demo

---
+++ pytests/test_demo1.py
@@ -0,0 +1,5 @@
+## Any pytest file should start with test_ or end with it _test
+
+## write all codes inside function. e.g. test_firstProgram() is a test method
+def test_firstProgram():
+    print("hello world") 
